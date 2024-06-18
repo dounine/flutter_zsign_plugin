08a59a353f021ea6074cb7344507823917e793aa
lake <102535481@qq.com>
2024-06-18 23:31:06 +0800

    init

--- ios/Classes/FlutterZsignPlugin.swift
+++ ios/Classes/FlutterZsignPlugin.swift
@@ -8,6 +8,10 @@ public class FlutterZsignPlugin: NSObject, FlutterPlugin {
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
+  func 建立虚拟函数表(){
+    md5(nil); //让xcode链接去找md5函数，否则静态库会报错
+  }
+
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
     case "getPlatformVersion":
--- macos/flutter_zsign_plugin.podspec
+++ macos/flutter_zsign_plugin.podspec
@@ -12,7 +12,6 @@ A new Flutter plugin project.
   s.homepage         = 'http://example.com'
   s.license          = { :file => '../LICENSE' }
   s.author           = { 'Your Company' => 'email@example.com' }
-
   s.source           = { :path => '.' }
   s.source_files     = 'Classes/**/*'
   s.vendored_libraries = "**/*.a"
